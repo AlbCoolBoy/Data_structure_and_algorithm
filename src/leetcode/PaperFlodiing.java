c041492c99f45367413182fe3274a668cd1a725e
MattShao <MattShao2022@outlook.com>
2022-11-13 11:22:13 +0800

    微软的折纸问题

---
+++ src/leetcode/PaperFlodiing.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/*微软的折纸问题*/
+public class PaperFlodiing {
+    public static void printAllFolds(int N){
+        process(1,N,true);
+        System.out.println();
+    }
+
+    public static void process(int i,int N,boolean down){
+        if(i>N){
+            return ;
+        }
+        process(i+1,N,true);
+        System.out.println(down?"凹":"凸");
+        process(i+1,N,false);
+    }
+
+    public static void main(String[] args) {
+        int N=4;
+        printAllFolds(N);
+    }
+}
