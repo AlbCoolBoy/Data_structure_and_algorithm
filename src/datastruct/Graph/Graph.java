80d2f031f2ffde77651b3638203c52b4441dd2b7
MattShao <MattShao2022@outlook.com>
2022-11-22 00:24:58 +0800

    根据任意给定数据定义方式，创建符合当前数据结构的图

--- src/datastruct/Graph/Graph.java
+++ src/datastruct/Graph/Graph.java
@@ -2,7 +2,7 @@ package datastruct.Graph;
 
 import java.util.HashMap;
 import java.util.HashSet;
-
+//图的数据结构定义的方式有很多种，将一种使用熟练，在遇到实际问题的时候将给定的数据结构转换到最常使用的数据结构上
 public class Graph {
     public HashMap<Integer,Node> nodes; //图的点集
     public HashSet<Edge> edges;         //图的边集
@@ -11,5 +11,31 @@ public class Graph {
         nodes=new HashMap<>();
         edges=new HashSet<>();
 
+    }
+    //给一个例子：
+    //如果给定的图的结构是一个矩阵，每一行的三个元素分别代表 边的起始点、边指向的终端节点、边的权重
+    //可以使用下面的方式将其转换为自己的数据结构
+    public static Graph createGraph(Integer[][] matrix){
+        Graph graph=new Graph();        //实例化一个图对象
+        for(int i=0;i<matrix.length;i++){
+            Integer from=matrix[i][0];
+            Integer to=matrix[i][1];
+            Integer weight=matrix[i][2];
+            if(!graph.nodes.containsKey(from)){
+                graph.nodes.put(from,new Node(from));   //添加节点
+            }
+            if(!graph.nodes.containsKey(to)){
+                graph.nodes.put(to,new Node(to));       //添加节点
+            }
+            Node fromNode=graph.nodes.get(from);
+            Node toNode=graph.nodes.get(to);
+            Edge newEdge=new Edge(weight,fromNode,toNode);
+            fromNode.nexts.add(toNode);
+            fromNode.out++;
+            toNode.in++;
+            fromNode.edges.add(newEdge);
+            graph.edges.add(newEdge);
+        }
+        return graph;
     }
 }
